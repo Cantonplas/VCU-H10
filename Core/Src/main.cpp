db1ba1404c0df9b163f58b6ecf9c673639474275
Catonplas <dcancat@etsinf.upv.es>
2025-02-14 00:38:02 +0100

    First draft

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -6,32 +6,36 @@
 
 #include "ST-LIB.hpp"
 
-uint32_t local_serversocket_port = 8202;
-std::string localip = "127.0.0.1";
-std::unique_ptr<ServerSocket> serversocket = nullptr;
-StackPacket* inputpacket = nullptr;
+
 
 int main(void) {
 #ifdef SIM_ON
     SharedMemory::start();
 #endif
+    uint32_t local_serversocket_port = 8202;
+    std::string localip = "127.0.0.1";
+    std::unique_ptr<ServerSocket> serversocket = nullptr;
+    StackPacket* inputpacket = nullptr;
     DigitalInput input(PA1);
     bool value = false;
     serversocket = std::make_unique<ServerSocket>(localip,local_serversocket_port);
     StackPacket* mypacket = new StackPacket(15,&value);//Random id
     STLIB::start();
 
-    Time::register_low_precision_alarm(100,[&](){
+    Time::register_low_precision_alarm(1000,[&](){
         if(mysocket->is_connected() == false){
             mysocket->reconnect();
         }
     });
 
+    Time::register_low_precision_alarm(200,[&](){
+        serversocket->send_packet(mypacket);
+    });
+
     while (1) {
         value = input.read_pin_state();
-        serversocket->send_packet(mypacket);
         STLIB::update();
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::this_thread::sleep_for(std::chrono::milliseconds(150));
     }
 }
 
